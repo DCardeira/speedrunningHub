6d09abe8d2e7b3c6e31794a32c194cfde4c71762
abeillemaia <joaovictormaia2004@hotmail.com>
2025-06-23 04:38:22 +0100

    ADDED: ASP.NET Core MVC backend implementation.

---
+++ app/backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SpeeRunningHub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
